0f6eb36acc8a54edfac9a3aed54767622f6c2d4f
micheaol <micheaol80@gmail.com>
2021-07-10 18:03:30 +0000

    more code adjustment on recursion

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 // create a global array of options: Rock-paper scissors
 const selection = ["rock", "paper", "scissors"];
+let totalHumanWin = 0;
+let totalComputerWin = 0;
 
 // ==========create computer player funtion =========================
 const computerPlayer = () => {
@@ -25,15 +27,44 @@ const humanPlayer = (str) => {
     // ==========End of human player funtion =========================
 
 // ==========create playGame funtion =========================
-const playGame = () => {
-    let totalHumanWin = 0;
-    let totalComputerWin = 0;
+const checkWinner = () => {
     let message = "";
     let humanInput = humanPlayer()
     let computerInput = computerPlayer();
+    if (humanInput === 'rock' && computerInput === 'scissors') {
+        console.log("You win")
+        totalHumanWin += 1;
+    } else if (humanInput === 'paper' && computerInput === 'rock') {
+        console.log("You win")
+        totalHumanWin += 1;
+    } else if (humanInput === 'scissors' && computerInput === 'paper') {
+        console.log("You win")
+        totalHumanWin += 1;
+    } else if (humanInput === computerInput) {
+        console.log("It's a tie")
+    } else {
+        console.log("Computer wins")
+        totalComputerWin += 1;
+    }
 
-    return `User is: ${humanInput} && computer is: ${computerInput}`;
 
-
+    // return totalHumanWin
 }
-console.log(playGame())
+
+const playGame = () => {
+    checkWinner();
+    if (totalHumanWin === 5 || totalComputerWin === 5) {
+
+    } else {
+        playGame();
+    }
+}
+doublePlay();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
+
+console.log(totalHumanWin);
+console.log(totalComputerWin);
