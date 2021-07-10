8950d6f991cc8db943308155c8e1375eeb210df7
micheaol <micheaol80@gmail.com>
2021-07-10 13:46:50 +0000

    More adjustment on my code

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,39 @@
-// create computer funtion which will include array of [Rock, Paper, and Scissors]
-// create human play funtion which will include array of [Rock, Paper, and Scissors]
-// Loop through the array to make selection
-// compare the selection
-// 
-// 
-// 
-// 
-// 
+// create a global array of options: Rock-paper scissors
+const selection = ["rock", "paper", "scissors"];
 
+// ==========create computer player funtion =========================
 const computerPlayer = () => {
-    let selection = ["Rock", "Paper", "Scissors"];
-    // Generate random number to match the array
-    let selectionGenerated = Math.round(Math.random() * 3);
-    // Loop the array of selection to make choice for the computer.
-    for (let i = 0; i < selection.length; i++) {
-        return selection[selectionGenerator];
+        // create a random generator to generate number
+        let numberGenerator = Math.round(Math.random() * 2);
+        // loop through the selection to make a choice for the computer
+        for (let i = 0; i < selection.length; i++) {
+            return selection[numberGenerator];
+        }
     }
+    // ==========End of computer player funtion =========================
+
+// ==========create Human player funtion =========================
+const humanPlayer = (str) => {
+        // receive input from client
+        do {
+            str = prompt("Enter your choice between: Paper, rock and scissors ");
+        } while (str === null);
+        let userInput = str.toLowerCase();
+        return userInput;
+        // return userInput;
+    }
+    // ==========End of human player funtion =========================
+
+// ==========create playGame funtion =========================
+const playGame = () => {
+    let totalHumanWin = 0;
+    let totalComputerWin = 0;
+    let message = "";
+    let humanInput = humanPlayer()
+    let computerInput = computerPlayer();
+
+    return `User is: ${humanInput} && computer is: ${computerInput}`;
+
 
 }
-console.log(computerPlayer());
+console.log(playGame())
