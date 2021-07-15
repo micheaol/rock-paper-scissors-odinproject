dc3cf2d5e8d0bb6784a19c9485758bd63689a11e
micheaol <micheaol80@gmail.com>
2021-07-15 19:25:42 +0000

    Correct logic for click event

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 const selection = ["rock", "paper", "scissors"];
 let totalHumanWin = 0;
 let totalComputerWin = 0;
+const buttons = document.querySelectorAll('button');
 
 // ==========create computer player funtion =========================
 const computerPlayer = () => {
@@ -14,64 +15,84 @@ const computerPlayer = () => {
     }
     // =============End of computer player funtion =========================
 
-// ==========create Human player funtion =========================
-const humanPlayer = (str) => {
-        // receive input from client
-        do {
-            str = prompt("Enter your choice between: Paper, rock and scissors ");
-        } while (str === null);
-        let userInput = str.toLowerCase();
-        return userInput;
-        // return userInput;
+
+// ==========create  humanPlayer funtion =========================
+const humanPlayer = () => {
+        let humanInput;
+        let computerInput = computerPlayer();
+        buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+                humanInput = button.id;
+
+                // console.log(userClick)
+                if (humanInput === 'rock' && computerInput === 'scissors') {
+                    console.log("You win")
+                    totalHumanWin += 1;
+                } else if (humanInput === 'paper' && computerInput === 'rock') {
+                    console.log("You win")
+                    totalHumanWin += 1;
+                } else if (humanInput === 'scissors' && computerInput === 'paper') {
+                    console.log("You win")
+                    totalHumanWin += 1;
+                } else if (humanInput === computerInput) {
+                    console.log("It's a tie")
+                } else {
+                    console.log("Computer wins")
+                    totalComputerWin += 1;
+                }
+            });
+        });
+
     }
     // ==========End of human player funtion =========================
 
 // ==========create checkWinner funtion to compare the selection of player=========================
-const checkWinner = () => {
-        let message = "";
-        let humanInput = humanPlayer()
-        let computerInput = computerPlayer();
-        if (humanInput === 'rock' && computerInput === 'scissors') {
-            console.log("You win")
-            totalHumanWin += 1;
-        } else if (humanInput === 'paper' && computerInput === 'rock') {
-            console.log("You win")
-            totalHumanWin += 1;
-        } else if (humanInput === 'scissors' && computerInput === 'paper') {
-            console.log("You win")
-            totalHumanWin += 1;
-        } else if (humanInput === computerInput) {
-            console.log("It's a tie")
-        } else {
-            console.log("Computer wins")
-            totalComputerWin += 1;
-        }
+// const checkWinner = () => {
+//         let message = "";
+//         let humanInput = humanPlayer()
+//         console.log(humanInput);
+//         let computerInput = computerPlayer();
+//         if (humanInput === 'rock' && computerInput === 'scissors') {
+//             console.log("You win")
+//             totalHumanWin += 1;
+//         } else if (humanInput === 'paper' && computerInput === 'rock') {
+//             console.log("You win")
+//             totalHumanWin += 1;
+//         } else if (humanInput === 'scissors' && computerInput === 'paper') {
+//             console.log("You win")
+//             totalHumanWin += 1;
+//         } else if (humanInput === computerInput) {
+//             console.log("It's a tie")
+//         } else {
+//             console.log("Computer wins")
+//             totalComputerWin += 1;
+//         }
 
-    }
-    // ========== End of checkWinner funtion to compare the selection of player=========================
+//     }
+// ========== End of checkWinner funtion to compare the selection of player=========================
 
 //======== Recursive function to call the play the game 5 number of times.
-// const playGame = () => {
-//     checkWinner();
-//     if (totalHumanWin === 5 || totalComputerWin === 5) {
+const playGame = () => {
+    humanPlayer();
+    if (totalHumanWin === 5 || totalComputerWin === 5) {
 
-//     } else {
-//         playGame();
-//     }
-// }
+    } else {
+        playGame();
+    }
+}
 
 //======= End of Recursive function to call the play the game number of  times.
-checkWinner();
+// checkWinner();
 
-console.log(totalHumanWin);
-console.log(totalComputerWin);
+playGame();
+// console.log(totalComputerWin);
 
-const buttons = document.querySelectorAll('button');
+// const buttons = document.querySelectorAll('button');
 
-buttons.forEach((button) => {
+// buttons.forEach((button) => {
 
-    // and for each one we add a 'click' listener
-    button.addEventListener('click', () => {
-        console.log(button.id)
-    });
-});
+//     // and for each one we add a 'click' listener
+//     button.addEventListener('click', () => {
+//         console.log(button.id)
+//     });
+// });
