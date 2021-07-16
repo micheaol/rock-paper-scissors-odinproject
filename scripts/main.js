7ab24d10b18eb3a1a9211fbcc25a00ef2154e3d9
micheaol <micheaol80@gmail.com>
2021-07-16 20:48:13 +0000

    Adjustment of random number generator

--- scripts/main.js
+++ scripts/main.js
@@ -10,111 +10,66 @@ let showReset = document.getElementById('reset-btn');
 showReset.style.visibility = 'hidden';
 
 // ==========create computer player funtion =========================
-
 const computerPlayer = () => {
-        let randomNumber = Math.floor(Math.random() * 1 + 2) + 1;
+        // create a random generator to generate number
+        let numberGenerator = Math.round(Math.random() * 2);
+        // loop through the selection to make a choice for the computer
         for (let i = 0; i < selection.length; i++) {
-            if (randomNumber === selection[i]) {
-                selection[randomNumber - 1]
-            }
-            return selection[randomNumber - 1]
+            return selection[numberGenerator];
         }
-
     }
     // =============End of computer player funtion =========================
 
-
-// ==========create  humanPlayer funtion =========================
+// ==========create Human player funtion =========================
 const humanPlayer = () => {
-        let message = "";
-        let humanInput;
-        let computerInput = computerPlayer();
+        let clickButton;
         buttons.forEach((button) => {
             button.addEventListener('click', () => {
-                humanInput = button.id;
+                clickButton = button.id;
+                console.log(clickButton);
+            })
 
-                // console.log(userClick)
-                if (humanInput === 'rock' && computerInput === 'scissors') {
-                    message = "You Win!!!"
-                        // console.log(message)
-                    totalHumanWin += 1;
-                } else if (humanInput === 'paper' && computerInput === 'rock') {
-                    message = "You Win!!!"
-                        // console.log(message)
-                    totalHumanWin += 1;
-                } else if (humanInput === 'scissors' && computerInput === 'paper') {
-                    message = "You Win!!!"
-                        // console.log(message)
-                    totalHumanWin += 1;
-                } else if (humanInput === computerInput) {
-                    message = "It's a tie!!!"
-                        // console.log(message)
-                } else {
-                    message = "Computer wins"
-                        // console.log(message)
-                    totalComputerWin += 1;
-                }
-                messageDisplay.textContent = message;
-                cpuScore.textContent = totalComputerWin;
-                humanScore.textContent = totalHumanWin;
-                if (totalHumanWin === 5 || totalComputerWin === 5) {
-                    button.disabled = true;
-                    showReset.style.visibility = 'visible';
-
-                    if (totalComputerWin > totalHumanWin) {
-                        messageDisplay.textContent = "OH Computer Wins"
-                    } else {
-                        messageDisplay.textContent = "Wow!! You won!!"
-                    }
-                }
-            });
         });
+
     }
     // ==========End of human player funtion =========================
 
 // ==========create checkWinner funtion to compare the selection of player=========================
-// const checkWinner = () => {
-//         let message = "";
-//         let humanInput = humanPlayer()
-//         console.log(humanInput);
-//         let computerInput = computerPlayer();
-//         if (humanInput === 'rock' && computerInput === 'scissors') {
-//             console.log("You win")
-//             totalHumanWin += 1;
-//         } else if (humanInput === 'paper' && computerInput === 'rock') {
-//             console.log("You win")
-//             totalHumanWin += 1;
-//         } else if (humanInput === 'scissors' && computerInput === 'paper') {
-//             console.log("You win")
-//             totalHumanWin += 1;
-//         } else if (humanInput === computerInput) {
-//             console.log("It's a tie")
-//         } else {
-//             console.log("Computer wins")
-//             totalComputerWin += 1;
-//         }
+const checkWinner = () => {
+        let message = "";
+        let humanInput = humanPlayer()
+        let computerInput = computerPlayer();
+        if (humanInput === 'rock' && computerInput === 'scissors') {
+            console.log("You win")
+            totalHumanWin += 1;
+        } else if (humanInput === 'paper' && computerInput === 'rock') {
+            console.log("You win")
+            totalHumanWin += 1;
+        } else if (humanInput === 'scissors' && computerInput === 'paper') {
+            console.log("You win")
+            totalHumanWin += 1;
+        } else if (humanInput === computerInput) {
+            console.log("It's a tie")
+        } else {
+            console.log("Computer wins")
+            totalComputerWin += 1;
+        }
 
-//     }
-// ========== End of checkWinner funtion to compare the selection of player=========================
+    }
+    // ========== End of checkWinner funtion to compare the selection of player=========================
 
-//======== Recursive function to call the play the game 5 number of times.
+//======== Recursive function to call the play the game number of times.
 const playGame = () => {
-    humanPlayer();
+    checkWinner();
+    if (totalHumanWin === 5 || totalComputerWin === 5) {
 
+    } else {
+        playGame();
+    }
 }
 
 //======= End of Recursive function to call the play the game number of  times.
-// checkWinner();
+humanPlayer();
 
-playGame();
+// console.log(totalHumanWin);
 // console.log(totalComputerWin);
-
-// const buttons = document.querySelectorAll('button');
-
-// buttons.forEach((button) => {
-
-//     // and for each one we add a 'click' listener
-//     button.addEventListener('click', () => {
-//         console.log(button.id)
-//     });
-// });
